0d6799ede95cef249997ee552fdd609b3292dc9b
Abdulrhman-J <a.jammaza@gmail.com>
2019-11-06 16:16:00 +0300

    Updates

---
+++ precache-manifest.6e07d9d5e55af3ebe8a90e0dd1678b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b25f7099ada490d6758b95bec679418",
+    "url": "/estra7a-app/index.html"
+  },
+  {
+    "revision": "093ab4b15fcbe821312f",
+    "url": "/estra7a-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b8c4bb0198877b89021d",
+    "url": "/estra7a-app/static/css/main.ab52e1c3.chunk.css"
+  },
+  {
+    "revision": "093ab4b15fcbe821312f",
+    "url": "/estra7a-app/static/js/2.af2f7bd7.chunk.js"
+  },
+  {
+    "revision": "b8c4bb0198877b89021d",
+    "url": "/estra7a-app/static/js/main.ac65c8d9.chunk.js"
+  },
+  {
+    "revision": "2804bc18452526835cf9",
+    "url": "/estra7a-app/static/js/runtime-main.8b5ef9c6.js"
+  }
+]);
